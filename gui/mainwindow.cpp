f5cdf95a298d014ead3478934004e4054208a4bf
murakaevaD <daniamurakaeva@yandex.ru>
2024-04-23 16:58:03 +0300

    setup ui

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -6,11 +6,32 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), boardSize(3) {
 }
 
 MainWindow::~MainWindow() {
-
+    for (auto &row : buttons) {
+        for (auto *button : row) {
+            delete button;
+        }
+    }
+    delete gameBoard;
+    delete resetButton;
+    delete winnerButton;
 }
 
 void MainWindow::setupUi() {
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+    QGridLayout *layout = new QGridLayout(centralWidget);
 
+    for (int i = 0; i < boardSize; i++) {
+        QVector<QPushButton*> buttonRow;
+        for (int j = 0; j < boardSize; j++) {
+            QPushButton *button = new QPushButton(" ", this);
+            button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+            layout->addWidget(button, i, j);
+            buttonRow.push_back(button);
+            connect(button, &QPushButton::clicked, this, [this, i, j] { handleButton(i, j); });
+        }
+        buttons.push_back(buttonRow);
+    }
 }
 
 void MainWindow::createGameBoard(int size) {
