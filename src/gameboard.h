1e67f8d0c7fe96042ea2e9d0d3f39f5ef8e1e7b8
murakaevaD <daniamurakaeva@yandex.ru>
2024-04-23 17:04:42 +0300

    new logic in Class GameBoard

--- src/gameboard.h
+++ src/gameboard.h
@@ -2,19 +2,33 @@
 #define GAMEBOARD_H
 
 #include <vector>
+#include <QString>
+
+class Player {
+public:
+    explicit Player(QString symbol) : symbol(symbol) {}
+
+    QString getSymbol() const { return symbol; }
+
+private:
+    QString symbol;
+};
 
 class GameBoard {
 public:
     explicit GameBoard(int size = 3);
     void reset();
-    bool makeMove(int row, int col, char player);
-    char getCurrentPlayer() const;
-    char checkWinner() const;
+    bool makeMove(int row, int col, Player* player);
+    Player* getCurrentPlayer() const;
+    Player* checkWinner() const;
+    void switchPlayers();
 
 private:
     int size;
-    std::vector<std::vector<char>> board;
-    char currentPlayer;
+    std::vector<std::vector<Player*>> board;
+    Player player1, player2;
+    Player* currentPlayer;
+
     bool checkRows() const;
     bool checkCols() const;
     bool checkDiagonals() const;
