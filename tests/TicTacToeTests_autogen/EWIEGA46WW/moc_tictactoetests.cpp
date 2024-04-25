bccba6a2627971369710e271836208ea696f734b
murakaevaD <daniamurakaeva@yandex.ru>
2024-04-25 18:16:52 +0300

    fully workable version of app

---
+++ tests/TicTacToeTests_autogen/EWIEGA46WW/moc_tictactoetests.cpp
@@ -0,0 +1,210 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'tictactoetests.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../TicTacToeGame/tests/tictactoetests.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'tictactoetests.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSTestGameBoardENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSTestGameBoardENDCLASS = QtMocHelpers::stringData(
+    "TestGameBoard",
+    "testHorizontalWin",
+    "",
+    "testVerticalWin",
+    "testDiagonalWin",
+    "testDraw1",
+    "testDraw2",
+    "testDraw3",
+    "testDraw4",
+    "testDraw5"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSTestGameBoardENDCLASS_t {
+    uint offsetsAndSizes[20];
+    char stringdata0[14];
+    char stringdata1[18];
+    char stringdata2[1];
+    char stringdata3[16];
+    char stringdata4[16];
+    char stringdata5[10];
+    char stringdata6[10];
+    char stringdata7[10];
+    char stringdata8[10];
+    char stringdata9[10];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSTestGameBoardENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSTestGameBoardENDCLASS_t qt_meta_stringdata_CLASSTestGameBoardENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 13),  // "TestGameBoard"
+        QT_MOC_LITERAL(14, 17),  // "testHorizontalWin"
+        QT_MOC_LITERAL(32, 0),  // ""
+        QT_MOC_LITERAL(33, 15),  // "testVerticalWin"
+        QT_MOC_LITERAL(49, 15),  // "testDiagonalWin"
+        QT_MOC_LITERAL(65, 9),  // "testDraw1"
+        QT_MOC_LITERAL(75, 9),  // "testDraw2"
+        QT_MOC_LITERAL(85, 9),  // "testDraw3"
+        QT_MOC_LITERAL(95, 9),  // "testDraw4"
+        QT_MOC_LITERAL(105, 9)   // "testDraw5"
+    },
+    "TestGameBoard",
+    "testHorizontalWin",
+    "",
+    "testVerticalWin",
+    "testDiagonalWin",
+    "testDraw1",
+    "testDraw2",
+    "testDraw3",
+    "testDraw4",
+    "testDraw5"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSTestGameBoardENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       8,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   62,    2, 0x08,    1 /* Private */,
+       3,    0,   63,    2, 0x08,    2 /* Private */,
+       4,    0,   64,    2, 0x08,    3 /* Private */,
+       5,    0,   65,    2, 0x08,    4 /* Private */,
+       6,    0,   66,    2, 0x08,    5 /* Private */,
+       7,    0,   67,    2, 0x08,    6 /* Private */,
+       8,    0,   68,    2, 0x08,    7 /* Private */,
+       9,    0,   69,    2, 0x08,    8 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject TestGameBoard::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_CLASSTestGameBoardENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSTestGameBoardENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSTestGameBoardENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<TestGameBoard, std::true_type>,
+        // method 'testHorizontalWin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testVerticalWin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDiagonalWin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDraw1'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDraw2'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDraw3'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDraw4'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testDraw5'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void TestGameBoard::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TestGameBoard *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->testHorizontalWin(); break;
+        case 1: _t->testVerticalWin(); break;
+        case 2: _t->testDiagonalWin(); break;
+        case 3: _t->testDraw1(); break;
+        case 4: _t->testDraw2(); break;
+        case 5: _t->testDraw3(); break;
+        case 6: _t->testDraw4(); break;
+        case 7: _t->testDraw5(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *TestGameBoard::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TestGameBoard::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSTestGameBoardENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int TestGameBoard::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 8;
+    }
+    return _id;
+}
+QT_WARNING_POP
