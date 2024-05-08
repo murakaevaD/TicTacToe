10a90b80c8356f95eba86c9785062ff5e76203cb
murakaevaD <daniamurakaeva@yandex.ru>
2024-05-08 15:55:03 +0300

    new logic to class MainWindow to add inf board to gui

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -4,14 +4,9 @@
 #include <QMainWindow>
 #include <QGridLayout>
 #include <QPushButton>
+#include <QMessageBox>
 #include <QSpinBox>
 #include "gameboard.h"
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -26,18 +21,17 @@ private slots:
     void startNewGame();
 
 private:
-    Ui::MainWindow *ui;
     GameBoard *board;
     QGridLayout *gridLayout;
     QPushButton *resetButton;
-    QSpinBox *sizeSpinBox;
     QPushButton *newGameButton;
     std::vector<std::vector<QPushButton*>> buttons;
+
     void setupInitialUI();
-    void createBoard();
+    void createBoard(int size, bool infinite);
     void updateButton(QPushButton *button, int row, int col);
     void clearBoard();
+    void setupGameOptions();
 };
-
 
 #endif
